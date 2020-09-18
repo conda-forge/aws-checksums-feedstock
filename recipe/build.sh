ea8a76c69048fb89de06b1981a11aca6779662cf
Uwe L. Korn <uwe.korn@quantco.com>
2020-09-18 19:23:47 +0200

    Set CMAKE_PREFIX_PATH

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ set -ex
 mkdir build
 pushd build
 cmake ${CMAKE_ARGS} -GNinja \
+  -DCMAKE_PREFIX_PATH=$PREFIX \
   -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
   -DCMAKE_INSTALL_LIBDIR=lib \
   -DCMAKE_BUILD_TYPE=Release \
